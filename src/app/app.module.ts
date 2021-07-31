94587bb54a64e148364e332ec3849e21dfc0aa50
Parker Gates <parkergates14@gmail.com>
2021-07-31 06:59:11 -0600

    HR TimeSheet initial commit and file system creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    TimesheetComponent,
+    AnalyticsComponent,
+    AnalyticsTableComponent,
+    TopNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
